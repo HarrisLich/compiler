81b73a158c504df7690c2ffba2e00f905aaeefb0
Harris Lichstein <harris.lichstein1@marist.edu>
2026-02-26 12:44:56 -0500

    fix: Bootstrap Hello World

--- src/core/bootstrap.ts
+++ src/core/bootstrap.ts
@@ -5,7 +5,7 @@ export const bootstrap = () => {
     logger.info('[core:bootstrap] Starting Compiler');
 
 
-    const source = 'print("Hello, World!")';
+    const source = 'print("hello world")$';
     const tokens = scanTokens(source);
 
     for (const token of tokens) {
